e26a9cf9a3cd02120d2a934d8e82ecc1fd7cbe03
ivan robin <ivan.robin@hotline.finance>
2026-05-02 16:03:00 +0300

    arrays decl

---
+++ Siddhartha/arrays_declarations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int Array1[3] = {1, 2, 3}; // size = 3, {1, 2, 3}
+
+    int Array2[3] = {0}; // size = 3, {0, 0, 0} all elements = 0
+
+    int PartArray[3] = {5, 4}; // size = 3, {5, 4, 0}
+
+    const int LENGTH = 3;
+    int Array3[LENGTH] = {0}; // size = 3, {0, 0, 0}  all elements = 0
+
+    int Array4[] = {6, 7}; // size = 2
+}
