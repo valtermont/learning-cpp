d380dd2f16235c747fcd282f93d0c4ad3070651d
ivan robin <ivan.robin@hotline.finance>
2026-06-16 17:10:24 +0300

    chapter 7 listing 5-6 arrays and functions

---
+++ Prata/chapter7/listing6_arrfun2.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+const int ArSize = 8;
+
+int sum_arr(int arr[], int n);
+
+int main()
+{
+    int cookies[ArSize] {1,2,4,8,16,32,64,128};
+    
+    std::cout << cookies << " = array address, ";
+    std::cout << sizeof cookies << " = sizeof cookies\n";
+    
+    int sum = sum_arr(cookies, ArSize);
+    std::cout << "Total cookies eaten: " << sum << '\n';
+
+    sum = sum_arr(cookies, 3); // we can sum only first 3 elements
+    std::cout << "First three eaters ate " << sum << " cookies.\n";
+
+    sum = sum_arr(cookies + 4, 4); // we can use pointer sum
+    std::cout << "Last four eaters ate " << sum << " cookies.\n";
+
+    return 0;
+}
+
+int sum_arr(int arr[], int n)
+{
+    int total {0};
+
+    // warning: sizeof on array function parameter will return size of 'int *' instead of 'int[]'
+    // [-Wsizeof-array-argument]
+    std::cout << arr << " = arr address, ";
+    std::cout << sizeof arr << " = sizeof arr\n";
+
+    for (int i{0}; i < n; i++)
+        total += arr[i];
+    
+    return total;
+}
